fbdb29099c5430b5e2c201a8a7910de9f0b8dc0c
unknown <kumargithub41@gmail.com>
2022-06-10 22:19:15 +0530

    adding the project

---
+++ src/test/java/com/testpackage/TestClass.java
@@ -0,0 +1,27 @@
+package com.testpackage;
+
+import org.testng.annotations.Test;
+
+public class TestClass {
+     @Test
+	 public void sum() {
+		System.out.println("Sum");
+	 }
+     
+     @Test
+	 public void mul() {
+		System.out.println("Multiply");
+	 }
+     
+     @Test
+	 public void divide() {
+		System.out.println("Divide");
+	 }
+     
+     @Test
+	 public void sub() {
+		System.out.println("Subtraction");
+	 }
+     
+     
+}
